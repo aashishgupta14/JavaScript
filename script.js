15d388db8dd518314b3cd12ad39de9c34abd10b4
aashishgupta14 <aashishgupta72216@gmail.com>
2025-08-19 21:47:28 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,165 @@
+
+
+console.log(true+"1");
+
+
+
+//function
+
+// function name(){
+//     console.log("aashish gupta")
+// }
+// name();
+// const add=(a,b)=>{
+//     c=a+b;
+//     return c
+// }
+// d=add(5,2)
+// console.log(d)
+
+// function sum(a,b){
+// c=a+b;
+// return c
+// }
+
+// sum(5,8)
+// console.log(c)
+
+//loops
+// let a="aashish gupta"
+// for(let i=1;i<=5;i++){
+//     console.log(a)
+// }
+
+//forinloop
+// for(key in object){
+//     //code
+// }
+
+// const marks={
+//     aashish:"50",
+//     aman:"70",
+//     rohit:"55"
+// }
+// for(key in marks){
+//     console.log(key)
+// }
+
+
+
+
+// for of loop
+// for(variable of iterable){
+// code
+// }
+
+// for(key of "aashish"){
+//     console.log(key)
+// }
+//  i=0;
+// while(i<3){
+//     console.log(a)
+//     i++;
+// }
+
+
+//conditional statement
+// if-statement
+// else-statement
+// else if -statement
+
+//logical operators
+ //and or not
+
+//ternary operators  condition?case1:case2
+// let marks=5;
+// marks>33?console.log("Pass"):console.log("Pass")
+
+
+//comparison operator [==,!=,===,!===,<,>,<=,>=,]
+// let a=52;
+// let b=41;
+// console.log(a==b)
+// console.log(a!=b)
+// let c=41;
+// let d=42;
+// console.log(c<=d)
+// console.log(c!==d)
+// console.log(c===d) //it check equal and its type 
+
+//assignment operators[=,+=,-=,*=,/=,%=,**=]
+
+// let a=5;
+// a=a+2;
+// console.log(a)
+// let b=10;
+// b%=2;
+// console.log(b)
+// let b=10;
+// b**=2;
+// console.log(b)
+
+//arithmetic operators [+,-,*,/,**,%,++,--]
+
+// let a=20;
+// let b=10;
+// console.log(a+b)
+// console.log(a-b)
+// console.log(a*b)
+// console.log(a/b)
+// console.log(a%b)
+// console.log(a**b)
+// console.log(a++)
+// console.log(a)
+// console.log(b--)
+// console.log(b)
+
+
+//Object
+// const student={
+//     name:"aashish",
+//     age:"20",
+//     roll:"20"
+// }
+// student["marks"]=70
+// console.log(student)
+// console.log(student["name"])
+
+//primitive data type
+// let a=50;
+// let b="apple"
+// let c=null;
+// let d=Symbol("hello world");
+// let e=undefined;
+// let f=true;
+// let g=BigInt("55544")
+
+// console.log(a)
+// console.log(b)
+// console.log(c)
+// console.log(d)
+// console.log(e)
+// console.log(f)
+// console.log(g)
+
+// console.log(typeof(a))
+// console.log(typeof(b))
+// console.log(typeof(c))
+// console.log(typeof(d))
+// console.log(typeof(e))
+// console.log(typeof(f))
+// console.log(typeof(g))
+
+
+// const a=6;
+// console.log(a) //  block level scope -const decleared only one time and assign one time 
+
+
+// let a=5;
+// a=9;  // let is block level scope it decleared only one time and assign multi time modern way
+// console.log(a)
+
+
+// var a=5;
+// var a=6;  // var is global scope and re decleared many time and it is old way 
+// console.log(a) 
