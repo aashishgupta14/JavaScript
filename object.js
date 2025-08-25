f83cdd662862d1134cba448e7f19be9cb42b9229
aashishgupta14 <aashishgupta72216@gmail.com>
2025-08-25 18:55:09 +0530

    object added

---
+++ object.js
@@ -0,0 +1,46 @@
+// let person={
+//     name:"aashish",
+//     age:21,
+//     roll:7
+// }
+// console.log(person);
+// console.log(person.name)
+// console.log(person.age)
+// let {name,age,roll}=person
+// console.log(name)
+// console.log(age)
+// console.log(roll)
+//add 
+// person.city="vapi" //add
+// person.city="valsad" //update
+// console.log(person);
+// delete person.city;
+// console.log(person)
+
+//object method (function inside object)
+
+// let student={
+//     name:"aman",
+//     greet:function(){
+//        return "Hello  "+ this.name
+//     }
+// }
+// console.log(student.greet())
+
+// nested object 
+
+// let student={
+//     name:"rahul",
+//     address:{
+//         city:"vapi",
+//         pin:233366
+//     }
+// }
+//for each loop
+// for(let key in student){
+//       console.log(key)
+//      console.log(key,student[key])
+//}
+// console.log(student.address.city)
+// console.log(student.name)
+// console.log(student["name"]);
